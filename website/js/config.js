db06b74670deaa38af2183fb8f9ca6598ecf1033
XIMMYzwsss <jayjax26466@gmail.com>
2026-08-27 18:54:41 +0100

    Update config.js

--- website/js/config.js
+++ website/js/config.js
@@ -1,6 +1,4 @@
-// Paste your Supabase Project URL and anon public key.
-// Supabase → Project Settings → API
 window.HEXXSTRAP = {
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: ""
+  SUPABASE_URL: "https://fptlwzrsbpltueuvwxde.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZwdGx3enJzYnBsdHVldXZ3eGRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc4NDczNDksImV4cCI6MjEwMzQyMzM0OX0.4BlcN0qu71GRDU2To_EIfSiz1-nLDBfrzATuZ3HRM4Q"
 };
